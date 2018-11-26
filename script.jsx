472fcf81cbe1ffbd0d19b2972a061ee273dd56de
zip <892284133@qq.com>
2018-11-26 20:25:51 +0800

    注释

--- script.jsx
+++ script.jsx
@@ -8,15 +8,19 @@ const counter = (state = 0, action) => {
             return state
     }
 }
-
+//将state保存在变量中， getState返回当前变量的值，
+//getState与单个对象上的dispatch函数项结合， subscribe就是reudx 的store
 const { createStore } = (reducer) => {
     let state
     let listeners = []
     const getState = () => state
+    //dispatch是更改内部状态的唯一办法
+    //每次更新state之后，都要通知时间监听器调用
     const dispatch = (action) => {
         state = reducer(state, action)
         listeners.forEach(listener => listener())
     }
+    //因为subscribe可以多次调用， 所以需要追踪所有的事件监听，将新的事件监听送入数组中，
     const subscribe = (listener) => {
         listeners.push(listener)
         return ()=> {
